add72ed9fbc75f76724453e9bb6ebc459985252e
Claudir Furlan <claudirsf@gmail.com>
2019-05-27 23:24:49 +0000

    seeds parte1 de dex,tipo1 e tipo2 ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,167 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Criando os dexes"
+
+Dex.create!(
+    number: "???"
+    )
+    
+Dex.create!(
+    number: "001"
+    )
+    
+Dex.create!(
+    number: "002"
+    )
+
+Dex.create!(
+    number: "003"
+    )
+    
+Dex.create!(
+    number: "004"
+    )
+    
+Dex.create!(
+    number: "005"
+    )
+    
+Dex.create!(
+    number: "006"
+    )
+    
+Dex.create!(
+    number: "007"
+    )
+    
+Dex.create!(
+    number: "008"
+    )
+    
+Dex.create!(
+    number: "009"
+    )
+    
+Dex.create!(
+    number: "010"
+    )
+    
+Dex.create!(
+    number: "011"
+    )
+    
+Dex.create!(
+    number: "012"
+    )
+
+puts "dex'es cadastradas  com sucesso"
+
+
+puts "Criando os tipos1"
+
+
+
+Tipo1.create!(
+    tipo:"Agua"
+)
+Tipo1.create!(
+    tipo:"Fogo"
+)
+Tipo1.create!(
+    tipo:"Terrestre"
+)
+Tipo1.create!(
+    tipo:"Grama"
+)
+Tipo1.create!(
+    tipo:"Pedra"
+)
+Tipo1.create!(
+    tipo:"Eletrico"
+)
+Tipo1.create!(
+    tipo:"Normal"
+)
+Tipo1.create!(
+    tipo:"Lutador"
+)
+Tipo1.create!(
+    tipo:"Venenoso"
+)
+Tipo1.create!(
+    tipo:"Inseto"
+)
+Tipo1.create!(
+    tipo:"Fantasma"
+)
+Tipo1.create!(
+    tipo:"Psiquico"
+)
+Tipo1.create!(
+    tipo:"Gelo"
+)
+Tipo1.create!(
+    tipo:"Dragão"
+)
+Tipo1.create!(
+    tipo:"Voador"
+)
+
+
+puts "tipo1s cadastradas  com sucesso"
+
+
+
+puts "Criando os tipos2"
+
+Tipo2.create!(
+    tipo:"---------"
+    )
+Tipo2.create!(
+    tipo:"Agua"
+)
+Tipo2.create!(
+    tipo:"Fogo"
+)
+Tipo2.create!(
+    tipo:"Terrestre"
+)
+Tipo2.create!(
+    tipo:"Grama"
+)
+Tipo2.create!(
+    tipo:"Pedra"
+)
+Tipo2.create!(
+    tipo:"Eletrico"
+)
+Tipo2.create!(
+    tipo:"Normal"
+)
+Tipo2.create!(
+    tipo:"Lutador"
+)
+Tipo2.create!(
+    tipo:"Venenoso"
+)
+Tipo2.create!(
+    tipo:"Inseto"
+)
+Tipo2.create!(
+    tipo:"Fantasma"
+)
+Tipo2.create!(
+    tipo:"Psiquico"
+)
+Tipo2.create!(
+    tipo:"Gelo"
+)
+Tipo2.create!(
+    tipo:"Dragão"
+)
+Tipo2.create!(
+    tipo:"Voador"
+)
+
+puts "tipos2 cadastradas  com sucesso"
