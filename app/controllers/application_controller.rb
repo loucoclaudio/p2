48b0c7345329c564c387b5e584f25e5f422d8d5c
Claudir Furlan <claudirsf@gmail.com>
2019-06-03 07:00:12 -0300

    OBRIGADO SENHOR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
     # http_basic_authenticate_with name: "admin", password: "nimda", except: [:index, :show]
     before_action :authenticate_user!
-
+    before_action :is_admin?, :only => [:edit, :detroy]
+    protect_from_forgery
+    helper_method :current_user, :signed_in?, :is_admin?
+  
+    def signed_in?
+      !!current_user
+    end
+  
+    def is_admin?
+        redirect_to root_path unless current_user.admin?
+    #   signed_in? ? current_user.admin : false
+    end
 end
