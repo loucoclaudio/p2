fdb8e2c00b387ca3a3826a1c7880636107e1e08c
Claudir Furlan <claudirsf@gmail.com>
2019-06-02 22:21:22 -0300

    migrando table e model users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-    http_basic_authenticate_with name: "admin", password: "nimda", except: [:index, :show]
+    # http_basic_authenticate_with name: "admin", password: "nimda", except: [:index, :show]
+    before_action :authenticate_user!
 
 end
